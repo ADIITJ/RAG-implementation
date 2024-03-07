42a2cf25b18006c92ae67f9b2fb90d3833e3bdd1
ADIITJ <b22ai045@iitj.ac.in>
2024-03-07 12:43:14 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -5,19 +5,12 @@ from nltk.corpus import stopwords
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-TfidfVec = TfidfVectorizer(stop_words='english')
-lemmer = nltk.stem.WordNetLemmatizer()
+
 # Download NLTK resources
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-
-
-query_text = "what is RAG"
-query_vector = TfidfVec.transform([query_text])
 
 uploaded = ['data.txt']
 
@@ -31,42 +24,16 @@ for filename in uploaded:
         sentences += sent_tokenize(raw)
         word_tokens += nltk.word_tokenize(raw)
 
+lemmer = nltk.stem.WordNetLemmatizer()
 def LemNormalize(text):
     tokens = nltk.word_tokenize(text.lower().translate(dict((ord(punct), None) for punct in string.punctuation)))
     return [lemmer.lemmatize(token) for token in tokens]
 
-
+TfidfVec = TfidfVectorizer(stop_words='english')
 tfidf_matrix = TfidfVec.fit_transform(sentences)
 
-
-
-def response(user_response):
-    robo_response = ''
-    user_response = user_response.lower()
-    sentences.append(user_response)
-    tfidf_matrix_new = TfidfVec.fit_transform(sentences)
-    vals = cosine_similarity(tfidf_matrix_new[-1], tfidf_matrix_new)
-    idx = vals.argsort()[0][-2]
-    flat = vals.flatten()
-    flat.sort()
-    req_tfidf = flat[-2]
-    if(req_tfidf == 0):
-        robo_response += " I am sorry, unable to understand you"
-    else:
-        closest_cluster = distances[0][1]
-        cluster_vector = cluster_vector(closest_cluster)
-        similarity = cosine_similarity(tfidf_matrix_new[-1], cluster_vector)[0][0]
-        sentences_in_cluster = clusters[closest_cluster]["sentences"]
-        robo_response += f"Based on your query, I found these sentences in the closest cluster: \n"
-        for s in sentences_in_cluster:
-            robo_response += s + "\n"
-    sentences.remove(user_response)
-    return robo_response
-
-def cluster_vector(cluster_id):
-    cluster = clusters[cluster_id]
-    vector = cluster["vector"]
-    return vector
+query_text = "example query"
+query_vector = TfidfVec.transform([query_text])
 
 kmeans = KMeans(n_clusters=5) # Choose the number of clusters that suits your data
 kmeans.fit(tfidf_matrix)
@@ -90,4 +57,29 @@ for cluster_id, cluster in clusters.items():
     distances.append((distance, cluster_id))
 distances.sort(key=lambda x: x[0], reverse=True)
 
+def cluster_vector(cluster_id):
+    cluster = clusters[cluster_id]
+    vector = cluster["vector"]
+    return vector
+
 print(response(query_text))
+
+def response(user_response):
+    robo_response = ''
+    user_response = user_response.lower()
+    sentences.append(user_response)
+    tfidf_matrix_new = TfidfVec.fit_transform(sentences)
+    vals = cosine_similarity(tfidf_matrix_new[-1], tfidf_matrix_new)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf == 0):
+        robo_response += " I am sorry, unable to understand you"
+    else:
+        closest_cluster = distances[0][1]
+        cluster_vector = cluster_vector(closest_cluster)
+        similarity = cosine_similarity(tfidf_matrix_new[-1], cluster_vector)[0][0]
+        sentences_in_cluster = clusters[closest_cluster]["sentences"]
+        robo_response += f"Based on your query, I found these sentences in the closest cluster: \n"
+        for s
