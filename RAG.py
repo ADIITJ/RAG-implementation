79e2cab3b6501d32d1d0111ac45d61a9cb24689b
ADIITJ <b22ai045@iitj.ac.in>
2024-04-05 21:10:01 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -61,22 +61,15 @@ for i in range(len(cluster_labels)):
 for cluster_id, cluster in clusters.items():
     cluster["vector"] /= len(cluster["sentences"])
 
-
-def response(query_text):
+def find_similar_sentences(query_text, cluster):
     query_vector = TfidfVec.transform([query_text])
-    
     distances = []
-    for cluster_id, cluster in clusters.items():
-        distance = cosine_similarity(query_vector, cluster["vector"])[0][0]
-        distances.append((distance, cluster_id))
+    for sentence in cluster["sentences"]:
+        sentence_vector = TfidfVec.transform([sentence])
+        distance = cosine_similarity(query_vector, sentence_vector)[0][0]
+        distances.append((distance, sentence))
     distances.sort(key=lambda x: x[0], reverse=True)
-
-    closest_cluster = distances[0][1]
-    sentences_in_cluster = clusters[closest_cluster]["sentences"]
-    robo_response = f"Based on your query, I found these sentences in the closest cluster: \n"
-    for s in sentences_in_cluster:
-        robo_response += f"- {s}\n"
-    return robo_response
+    return [sentence for _, sentence in distances[:3]]  # Return top 3 similar sentences
 
 query_text = ""
 while query_text.lower() != "bye":
